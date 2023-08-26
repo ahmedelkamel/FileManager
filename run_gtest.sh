ef411f48ccd280e9299192ee6658df9a7379a7e7
Ahmed Elkamel <eeahmedelkamel@gmail.com>
2023-08-26 17:31:22 +0300

    ResourceManager: update build script

--- run_gtest.sh
+++ run_gtest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Generate config files into the build dir
-cmake -B /home/$USER/code/gtest/build -S /home/$USER/code/gtest/c
+cmake -B /home/$USER/code/ResourceManager/build -S /home/$USER/code/ResourceManager/
 # Build the project given the config specified in build dir
-if cmake --build /home/$USER/code/gtest/build; then
+if cmake --build /home/$USER/code/ResourceManager/build; then
     # Run the executable
-    /home/$USER/code/gtest/build/test/test_runner
+    /home/$USER/code/ResourceManager/build/test/test_runner
 fi
